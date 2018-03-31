f7707a34b8ca9e7667826d055e2f61a25b78cebc
Evgeny Vedegis <tichoo@yandex.ru>
2018-03-31 15:31:10 +0300

    Design fix

--- dev/js/index.js
+++ dev/js/index.js
@@ -12,10 +12,8 @@ new App({
 			{red: 79, green: 163, blue: 252},
 			{red: 104, green: 178, blue: 91},
 			{red: 233, green: 178, blue: 91},
-			{red: 104, green: 38, blue: 91},
-			{red: 104, green: 38, blue: 22},
-			{red: 11, green: 200, blue: 120},
-			{red: 56, green: 38, blue: 91}
+			{red: 121, green: 100, blue: 200},
+			{red: 106, green: 18, blue: 177}
 		]
 	})
 	
